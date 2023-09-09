60fa5089cb8932ea8085c30a26db89bc9a8cc5c5
Kelvin Bianchi <kelvinbianchi@Kelvins-MacBook-Air.local>
2023-09-09 10:29:36 +0200

    feat: initial commit

---
+++ cafeteria-finder/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+    <v-form>
+        <v-container>
+            <v-col cols="12">
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-text-field :counter="10" label="First name" required hide-details></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-text-field :counter="10" label="Last name" hide-details required></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-text-field label="E-mail" hide-details required></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-container>
+    </v-form>
+</template>
