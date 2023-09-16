a516e9975efa0f7cf8ad5ddc4333963c7ec6dd92
gahgsp <kelvinbianchi@gmail.com>
2023-09-16 15:57:00 +0200

    feat: styling header and footer

--- cafeteria-finder/src/App.vue
+++ cafeteria-finder/src/App.vue
@@ -29,20 +29,20 @@ function isCurrentPath(path: string) {
 
 <template>
   <v-app style="background-color: rgba(255, 218, 185, 0.1);">
-    <v-toolbar v-if="isLoggedIn">
-      <v-toolbar-title>Bem vindo, Kelvin!</v-toolbar-title>
+    <v-toolbar v-if="isLoggedIn" style="background-color: #F4978E;">
+      <v-toolbar-title style="color: #FFF">Bem vindo, Kelvin!</v-toolbar-title>
     </v-toolbar>
     <RouterView />
     <v-bottom-navigation v-if="isLoggedIn">
-      <v-btn value="finder" @click="onClickFinder" :disabled="isCurrentPath('/finder')">
+      <v-btn value="finder" @click="onClickFinder" :disabled="isCurrentPath('/finder')" style="color: #F4978E;">
         <v-icon>mdi-map-marker</v-icon>
         <span>Pesquisar</span>
       </v-btn>
-      <v-btn value="favorites" @click="onClickFavorites" :disabled="isCurrentPath('/favorites')">
+      <v-btn value="favorites" @click="onClickFavorites" :disabled="isCurrentPath('/favorites')" style="color: #F4978E;">
         <v-icon>mdi-heart</v-icon>
         <span>Favoritos</span>
       </v-btn>
-      <v-btn value="settings" @click="onClickSettings" :disabled="isCurrentPath('/settings')">
+      <v-btn value="settings" @click="onClickSettings" :disabled="isCurrentPath('/settings')" style="color: #F4978E;">
         <v-icon>mdi-cog</v-icon>
         <span>Configuraçōes</span>
       </v-btn>
