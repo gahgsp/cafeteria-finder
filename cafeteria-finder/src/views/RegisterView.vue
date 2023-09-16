ecddc2de24f3ae36eba3843cb2f3b006d509c7ee
gahgsp <kelvinbianchi@gmail.com>
2023-09-16 15:52:23 +0200

    feat: styling register view

--- cafeteria-finder/src/views/RegisterView.vue
+++ cafeteria-finder/src/views/RegisterView.vue
@@ -16,6 +16,8 @@ const confirmEmail = ref('')
 const password = ref('')
 const confirmPassword = ref('')
 
+const isPasswordVisible = ref(false)
+
 async function onSubmit() {
     const newUser: User = {
         name: toValue(name),
@@ -36,44 +38,52 @@ async function onSubmit() {
                 <v-col cols="12">
                     <v-row>
                         <v-col cols="12" md="12">
-                            <span class="text-h2">Create Account</span>
+                            <span class="text-h2" style="color: #F08080;">Registrar</span>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" md="12">
-                            <v-text-field v-model="name" label="Username" hide-details required></v-text-field>
+                            <v-text-field v-model="name" label="Usuário" hide-details required density="compact"
+                                variant="outlined"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" md="12">
-                            <v-text-field v-model="email" label="E-Mail" hide-details required></v-text-field>
+                            <v-text-field v-model="email" label="E-Mail" hide-details required density="compact"
+                                variant="outlined"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" md="12">
-                            <v-text-field v-model="confirmEmail" label="Confirm E-Mail" hide-details
-                                required></v-text-field>
+                            <v-text-field v-model="confirmEmail" label="Confirmar E-Mail" hide-details required
+                                density="compact" variant="outlined"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" md="12">
-                            <v-text-field v-model="password" label="Password" type="password" hide-details
-                                required></v-text-field>
+                            <v-text-field v-model="password" label="Senha" hide-details required
+                                :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                :type="isPasswordVisible ? 'text' : 'password'" variant="outlined"
+                                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                                density="compact"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" md="12">
-                            <v-text-field v-model="confirmPassword" label="Confirm Password" type="password" hide-details
-                                required></v-text-field>
+                            <v-text-field v-model="confirmPassword" label="Confirmar Senha" hide-details required
+                                :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                :type="isPasswordVisible ? 'text' : 'password'" variant="outlined"
+                                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                                density="compact"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="12" style="display: flex; justify-content: space-around; align-items: center;">
-                            <v-btn variant="outlined" type="submit" @click="onSubmit">
-                                Register
+                            <v-btn variant="tonal" type="submit" @click="onSubmit" size="large" style="color:#F4978E;">
+                                Registrar
                             </v-btn>
-                            <v-btn variant="outlined">
-                                Cancel
+                            <v-btn variant="tonal" size="large" style="color:#F8AD9D;">
+                                Cancelar
                             </v-btn>
                         </v-col>
                     </v-row>
