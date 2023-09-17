be79f03bfc53a6bbd0cd40bfe7daf06c495d5bee
gahgsp <kelvinbianchi@gmail.com>
2023-09-17 13:40:31 +0200

    feat: splitting Setting View into smaller components

---
+++ cafeteria-finder/src/components/SettingsMenu.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const emits = defineEmits(['onDeleteUser', 'onLogout'])
+
+const handleOnDeleteUser = () => {
+  emits('onDeleteUser')
+}
+
+const handleOnLogout = () => {
+  emits('onLogout')
+}
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-col cols="12" style="padding-bottom: 0">
+            <div><span class="text-h5" style="color: #f08080">Configurações</span></div>
+            <v-divider style="margin-top: 12px; margin-bottom: 12px" />
+          </v-col>
+          <v-col cols="12" style="padding-top: 0">
+            <v-list density="compact">
+              <v-list-item link @click="handleOnDeleteUser">
+                <v-list-item-title class="text-body-1">Excluir Conta</v-list-item-title>
+                <template v-slot:append>
+                  <v-avatar color="#FFDAB9">
+                    <v-icon color="white">mdi-delete</v-icon>
+                  </v-avatar>
+                </template>
+              </v-list-item>
+              <v-divider />
+              <v-list-item link @click="handleOnLogout">
+                <v-list-item-title class="text-body-1 font-weight-bold"
+                  >Encerrar Sessão</v-list-item-title
+                >
+                <template v-slot:append>
+                  <v-avatar color="#F08080">
+                    <v-icon color="white">mdi-logout</v-icon>
+                  </v-avatar>
+                </template>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- cafeteria-finder/src/views/SettingsView.vue
+++ cafeteria-finder/src/views/SettingsView.vue
@@ -1,57 +1,27 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import { useUserStore } from '@/stores/user';
+import SettingsMenu from '@/components/SettingsMenu.vue'
+import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const userStore = useUserStore()
 
-function handleLogout() {
+function handleOnLogout() {
+  userStore.logout()
+  router.push('/login')
+}
+
+async function handleOnDeleteUser() {
+  const { isError } = await userStore.deleteUser(userStore.currentUser!)
+  if (!isError) {
     userStore.logout()
     router.push('/login')
+  }
 }
-
-async function handleDeleteUser() {
-    const { isError } = await userStore.deleteUser(userStore.currentUser!)
-    if (!isError) {
-        userStore.logout()
-        router.push('/login')
-    }
-}
-
 </script>
 
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12">
-                <v-card>
-                    <v-col cols="12" style="padding-bottom: 0">
-                        <div><span class="text-h5" style="color: #F08080;">Configurações</span></div>
-                        <v-divider style="margin-top: 12px; margin-bottom: 12px;" />
-                    </v-col>
-                    <v-col cols="12" style="padding-top: 0;">
-                        <v-list density="compact">
-                            <v-list-item link @click="handleDeleteUser">
-                                <v-list-item-title class="text-body-1">Excluir Conta</v-list-item-title>
-                                <template v-slot:append>
-                                    <v-avatar color="#FFDAB9">
-                                        <v-icon color="white">mdi-delete</v-icon>
-                                    </v-avatar>
-                                </template>
-                            </v-list-item>
-                            <v-divider />
-                            <v-list-item link @click="handleLogout">
-                                <v-list-item-title class="text-body-1 font-weight-bold">Encerrar Sessão</v-list-item-title>
-                                <template v-slot:append>
-                                    <v-avatar color="#F08080">
-                                        <v-icon color="white">mdi-logout</v-icon>
-                                    </v-avatar>
-                                </template>
-                            </v-list-item>
-                        </v-list>
-                    </v-col>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-main>
+    <SettingsMenu @on-logout="handleOnLogout" @on-delete-user="handleOnDeleteUser" />
+  </v-main>
 </template>
