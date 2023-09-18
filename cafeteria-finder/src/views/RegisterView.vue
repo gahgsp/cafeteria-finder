e75501d2521001f047d92acb96d6b02a1285e834
gahgsp <kelvinbianchi@gmail.com>
2023-09-18 19:30:12 +0200

    feat: added error feedback to register view

--- cafeteria-finder/src/views/RegisterView.vue
+++ cafeteria-finder/src/views/RegisterView.vue
@@ -2,11 +2,13 @@
 import RegisterForm from '@/components/RegisterForm.vue'
 import { useUserStore } from '@/stores/user'
 import type { User } from '@/types'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-
 const userStore = useUserStore()
+
+const isErrorAlert = ref(false)
 
 const handleOnSubmit = async (formData: { name: string; email: string; password: string }) => {
   const newUser: User = {
@@ -28,5 +30,15 @@ const handleOnCancel = () => {
 <template>
   <main>
     <RegisterForm @on-submit="handleOnSubmit" @on-cancel="handleOnCancel" />
+    <v-snackbar v-model="isErrorAlert" timeout="5000" color="#F08080" variant="tonal">
+      <span>Um erro ocorreu!</span>
+      <br />
+      <span>Houve um erro ao registrar seu usuário.</span>
+      <br />
+      <span>Verifique as informações e tente novamente mais tarde.</span>
+      <template v-slot:actions>
+        <v-btn color="red" variant="text" @click="isErrorAlert = false"> Fechar </v-btn>
+      </template>
+    </v-snackbar>
   </main>
 </template>
