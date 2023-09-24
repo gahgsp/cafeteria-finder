84d08d42ad932df8b2f840903f4b006a4d918205
gahgsp <kelvinbianchi@gmail.com>
2023-09-24 17:25:11 +0200

    feat: adding loader and css classes

--- cafeteria-finder/src/views/FavoritesView.vue
+++ cafeteria-finder/src/views/FavoritesView.vue
@@ -11,13 +11,16 @@ const favoritesStore = useFavoritesStore()
 const mapStore = useMapStore()
 
 const favoriteCoffeeShops = ref<CoffeeShop[]>([])
+const isLoadingFavorites = ref<boolean>(false)
 
 onMounted(async () => {
+  isLoadingFavorites.value = true
   const { favorites } = await favoritesStore.loadFavorites()
   const { coffeeShops } = await mapStore.loadSpecificCoffeeShops(
     favorites.map((favorite) => favorite.coffeeShopId)
   )
   favoriteCoffeeShops.value = coffeeShops
+  isLoadingFavorites.value = false
 })
 
 const onNavigateToFavorite = async (coffeeShop: CoffeeShop) => {
@@ -32,10 +35,17 @@ const onNavigateToFavorite = async (coffeeShop: CoffeeShop) => {
       <v-col cols="12">
         <v-card>
           <v-col cols="12" style="padding-bottom: 0">
-            <div><span class="text-h5" style="color: #f08080">Meus Favoritos</span></div>
+            <div><span class="text-h5 primary">Meus Favoritos</span></div>
             <v-divider style="margin-top: 12px; margin-bottom: 12px" />
           </v-col>
-          <v-col cols="12" style="padding-top: 0">
+          <v-col cols="12" v-if="isLoadingFavorites">
+            <v-container>
+              <div class="text-center">
+                <v-progress-circular :size="100" indeterminate class="primary" />
+              </div>
+            </v-container>
+          </v-col>
+          <v-col cols="12" class="main" v-else>
             <v-alert color="red" variant="outlined" v-if="!favoriteCoffeeShops.length">
               <template v-slot:title> Nenhum Favorito Encontrado </template>
               Aparentemente você não possui cafeterias favoritas. ):<br />
@@ -47,7 +57,7 @@ const onNavigateToFavorite = async (coffeeShop: CoffeeShop) => {
                 :key="coffeeShop.id"
                 :title="coffeeShop.properties.name"
                 :subtitle="coffeeShop.properties.address"
-                style="padding-left: 0px"
+                class="favorite-item"
               >
                 <template v-slot:prepend>
                   <v-avatar color="grey-lighten-1">
@@ -70,3 +80,17 @@ const onNavigateToFavorite = async (coffeeShop: CoffeeShop) => {
     </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.primary {
+  color: #f08080;
+}
+
+.main {
+  padding-top: 0;
+}
+
+.favorite-item {
+  padding-left: 0px;
+}
+</style>
